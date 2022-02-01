14d2afc03e3ab48c2949a17c6e31394f4fa5c0ad
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-02-01 17:43:51 +0300

    remove unused libraries

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
